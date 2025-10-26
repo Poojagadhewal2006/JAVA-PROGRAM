8afee85110fe502bb8233ed40c860ab9c8e0242b
Poojagadhewal2006 <poojagadhewal2006@gmail.com>
2025-10-26 13:09:21 +0530

    Add files via upload

---
+++ Dynamic2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student implements Comparable <Student>
+{
+       int age;
+       String name;
+
+       public Student(int age,String name)
+       {
+        this.age=age;
+        this.name=name;
+       }
+       public String toString()
+       {
+        return age + " " + name;
+       }
+       public int compareTo(Student s)
+       {
+         return this.age-s.age;
+       }
+}
+public class Dynamic2 {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(23,"pooja"));
+        list.add(new Student(12,"harshita"));
+        Collections.sort(list);
+        for(Student s :list)
+        {
+           System.out.println(s.toString());
+        }
+    }
+} 
+
