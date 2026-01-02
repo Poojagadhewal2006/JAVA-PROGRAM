7ab90a3a456aaae4066214e3cb2767ad1d40af70
Poojagadhewal2006 <poojagadhewal2006@gmail.com>
2026-01-02 11:31:37 +0530

    Add files via upload

---
+++ Itration5.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Itration5 {
+    public static void main(String[] args) {
+        ArrayList <Integer> names = new ArrayList<>();
+        names.add(1);
+        names.add(10);
+        names.add(0);
+        names.add(12);
+        names.add(21);
+     //   Collections.shuffle(1);
+     //   Collections.sort(1);
+        System.out.println(names);
+    }
+}
